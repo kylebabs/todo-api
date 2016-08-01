abc2da04fa8beb750cb5fd55d2bfc4c49c502900
Kyle Babinowich <kbabinowich@gmail.com>
2016-08-01 13:22:30 -0400

    final static release

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res){
 	res.send('Todo API Root');
 });
 
-//GET /todos?completed=true
+//GET /todos?completed=true&q=house
 app.get('/todos', function(req, res){
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -26,6 +26,13 @@ app.get('/todos', function(req, res){
 	//if has property && completed === 'true'
 	// filteredTodos == _.where(filteredTodos, {completed: true})
 	// else if has prop && completed ==='false'
+
+	if(queryParams.hasOwnProperty('q')&& queryParams.q.length > 0 ){
+		filteredTodos = _.filter(filteredTodos, function(todo){
+			return todo.description.indexOf(queryParams.q) > -1;
+		});
+	}
+	//q prop exist, length greater than 0
 
 	res.json(filteredTodos);
 });
